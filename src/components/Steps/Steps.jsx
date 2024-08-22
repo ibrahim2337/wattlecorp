05e82694253b162dde64bff3d422a25766861109
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2024-08-22 13:29:11 +0600

    project responcive ready

--- src/components/Steps/Steps.jsx
+++ src/components/Steps/Steps.jsx
@@ -4,7 +4,7 @@ const Steps = () => {
     return (
         <div className="bg-[#121111] lg:px-12 px-5 pb-16">
             <h1 className="text-white text-center text-xl font-bold py-16">Steps Involved in Wattlecorp Penetration Testing Services In UAE</h1>
-            <div className="grid lg:grid-cols-6 grid-cols-2 gap-5">
+            <div className="grid lg:grid-cols-6 md:grid-cols-3 grid-cols-2 gap-5">
             <div className="bg-[#2B2626] p-4">
                 <div className="text-white flex justify-center bg-blue-600 mx-12 rounded-full">
                 <GiClassicalKnowledge className="lg:h-16 h-10 w-10 p-1" />
@@ -54,7 +54,7 @@ const Steps = () => {
             </div>
             </div>
 
-            <div className="grid lg:grid-cols-5 gap-5 mt-5">
+            <div className="grid lg:grid-cols-5 md:grid-cols-2 gap-5 mt-5">
             <div className="bg-[#2B2626] p-4">
                 <div className="text-white flex justify-center bg-blue-600 mx-16 rounded-full">
                 <GiClassicalKnowledge className="lg:h-20 h-10 w-14 p-1" />
