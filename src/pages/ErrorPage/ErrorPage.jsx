ccc54dc7f74d479863b2963261555b83d503cbe6
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2024-09-09 19:14:04 +0600

    project responcive ok

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Link } from "react-router-dom";
 import errorImg from "../../assets/error/view-3d-boy-using-laptop-removebg-preview.png";
 
